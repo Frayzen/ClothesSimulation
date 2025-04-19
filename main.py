c171aca3df6d77395c1c0927e17c7db170ad9e5b
florian.laine <florian.laine@epita.fr>
2025-04-19 21:37:26 +0200

    wip

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame, sys
 import numpy as np
 from numpy import *
 from pygame.locals import *
+from pygame.time import Clock
 
 from params import *
 
@@ -14,8 +15,11 @@ init()
 screen=display.set_mode((w_screen, h_screen),0,32)
 
 WHITE=(255,255,255)
-BLUE=(0,0,255)
 RED=(255, 0, 0)
+GREEN=(0,255,0)
+BLUE=(0,0,255)
+
+clock = Clock()
 
 w_nodes = side * nb_x
 h_nodes = side * nb_y
@@ -24,6 +28,12 @@ mid = array([w_screen, h_screen]) / 2
 from_pos = mid - node_size / 2
 
 pts=np.zeros((nb_y, nb_x, 2))
+vels=np.zeros((nb_y, nb_x, 2))
+weights=np.ones((nb_y, nb_x))
+
+# Pin the top corner ones
+weights[0,0] = inf
+weights[0,-1] = inf
 
 for i in range(nb_x):
     for j in range(nb_y):
@@ -61,6 +71,38 @@ while True:
 
     if mouse.get_pressed()[0]:
         pts[curids] = mpos
+        weights[curids] = inf
+
+    # Update pos
+    dt = clock.tick(FPS) / 150
+    for i in range(nb_x):
+        for j in range(nb_y):
+            vels[j, i][1] += dt * g * 1 / weights[j, i]
+            p = pts[j,i]
+            pts[j,i] = pts[j,i] + dt * vels[j,i]
+
+
+    # For all edges
+    for i in range(nb_x - 1):
+        for j in range(nb_y - 1):
+            p1, w1 = pts[j, i], weights[j,i]
+            p2, w2 = pts[j + 1, i], weights[j + 1, i]
+            p3, w3 = pts[j, i + 1], weights[j, i + 1]
+
+            # Vertical edge
+            dist1 = norm(p1 - p2)
+            e1 = (side - dist1)*(p1 - p2) / dist1
+            # draw.line(screen, )
+            # pts[j,i] += e1 * -w1 / (w1 + w2)
+            # pts[j + 1,i] += e1 * -w2 / (w1 + w2)
+
+            # Horizontal edge
+            dist2 = norm(p1 - p3)
+            e2 = (side - dist2)*(p1 - p3) / dist2
+            # pts[j,i] += e2 * -w1 / (w1 + w3)
+            # pts[j,i+1] += e2 * -w3 / (w1 + w3)
+
+
 
 
     display.update()
--- params.py
+++ params.py
@@ -4,3 +4,5 @@ h_screen = 600
 side = 50
 nb_x = 5
 nb_y = 5
+g = 9.8
+FPS = 60
